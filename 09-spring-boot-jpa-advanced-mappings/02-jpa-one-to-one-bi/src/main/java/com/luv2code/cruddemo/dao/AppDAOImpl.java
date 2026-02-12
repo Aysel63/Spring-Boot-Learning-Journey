6b1c1b4c3a3eba521e83ae5a73b417b88ef8e2a5
user <aysel.abdulzade05@gmail.com>
2026-02-12 22:17:12 +0400

    feat: implement bi-directional @OneToOne mapping

--- 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -1,6 +1,7 @@
 package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,10 @@ public class AppDAOImpl implements AppDAO{
 
 
 
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class,theId);
     }
 }
